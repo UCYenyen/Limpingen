16dca4d4760021002f604a65151019f893ac763e
Obie Zuriel <obiezuriel631@gmail.com>
2025-10-19 14:02:15 +0700

    Fix Footer and link

--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -1,13 +1,9 @@
-<div class="d-flex flex align-items-center gap-4 w-100 p-4 rounded-lg shadow-lg">
-    <img src="/images/projects/mysabda.svg" style="width: 30%;" height="100" class="cover" alt="project image">
+<div class="d-flex flex-column flex-md-row align-items-center gap-4 w-100 p-4 rounded-lg shadow-lg">
+    <img src="/images/projects/mysabda.svg" height="100" class="project-image cover" alt="project image">
     <div class="d-flex flex-column gap-4 justify-content-start" style="width: 60%">
         <h2 class="fs-2 fw-bold text-black">MySabda</h2>
         <p class="fs-5 fw-normal text-justify text-black">
-            MySabda is a comprehensive mobile application designed to facilitate easy access to religious texts and resources.
-            It offers features such as daily verses, prayer times, and community forums, making it a one-stop solution for spiritual growth
-            and connection.
-
-            We build powerful, custom-designed websites and applications to help your business succeed. Based in Tanjung Pinang, we partner with clients across Indonesia and around the world to bring their digital vision to life.
+            Developed a dynamic Bible Study App that brings Scripture to life through rich hyperlinking — connecting verses to commentaries, cross-references, and even original Hebrew and Greek texts.
         </p>
     </div>
 </div>
