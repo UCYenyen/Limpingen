fe95cb56eead34bc9498cd59e7274100834816c8
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-10-20 09:58:16 +0700

    fix typo on delete

--- database/migrations/2025_10_19_064330_create_packages_table.php
+++ database/migrations/2025_10_19_064330_create_packages_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('description');
             $table->string('price')->default(0);
-            $table->foreignId('service_id')->constrained('services')->onDeelete('cascade');
+            $table->foreignId('service_id')->constrained('services')->onDelete('cascade');
         });
     }
 
